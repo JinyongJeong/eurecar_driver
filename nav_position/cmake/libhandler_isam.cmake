7a367a6063bfac5d757028b167bbe1868933c563
irapkaist <irapkaist@gmail.com>
2019-12-04 13:04:37 +0900

    Update

--- nav_position/cmake/libhandler_isam.cmake
+++
@@ -1,6 +0,0 @@
-macro (libhandler_isam)
-  libhandler_find_library (isam "did you build isam in third-party?" ${ARGN})
-  if (ISAM_FOUND)
-    set (IRPLIB_ISAM ${ISAM_LIBRARIES})
-  endif ()
-endmacro ()
